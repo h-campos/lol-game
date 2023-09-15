927d001cf591600f9a16ae5f96f97128f1579b12
hcampos <88628151+Sakoutecher@users.noreply.github.com>
2023-09-15 16:49:43 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+const Page = (): ReactElement => {
+  return (
+    <h1 className="text-black dark:text-white text-4xl text-center">Hello World</h1>
+  );
+};
+
+export default Page;
