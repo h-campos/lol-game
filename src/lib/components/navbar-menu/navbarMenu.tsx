175ffeebfade66a94214a93d48179a58b316ba1e
hcampos <hcampospro@gmail.com>
2023-09-19 18:08:20 +0200

    feat(navbar): add navbar menu component to the navbar for improved user navigation and functionality feat(navbar): add LOLGAMES logo and navbar menu component to the navbar for improved user navigation and functionality

---
+++ src/lib/components/navbar-menu/navbarMenu.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import type { ReactElement } from "react";
+import { Button } from "@/lib/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/lib/components/ui/dropdown-menu";
+import Link from "next/link";
+import { HamburgerMenuIcon, ExitIcon, Pencil1Icon } from "@radix-ui/react-icons";
+
+export const NavbarMenu = (): ReactElement => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          <HamburgerMenuIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup>
+          <Link href="/blurry-champions">
+            <DropdownMenuItem>
+                Blurry Champions
+            </DropdownMenuItem>
+          </Link>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Pencil1Icon className="mr-2" /> Make a proposal
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <ExitIcon className="mr-2" /> Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/lib/components/navbar/navbar.tsx
+++ src/lib/components/navbar/navbar.tsx
@@ -1,7 +1,11 @@
 import type { ReactElement } from "react";
+import { NavbarMenu } from "@/lib/components/navbar-menu/navbarMenu";
 
 export const Navbar = (): ReactElement => {
   return (
-    <div className="text-white bg-red-500 w-full mb-6">navbar</div>
+    <div className="text-white bg-red-500 w-full mb-6 flex justify-between items-center">
+      <span style={{ filter: "drop-shadow(0px 5px 12px #ffffff)" }} className="text-2xl font-bold text-titleHighlight">LOLGAMES</span>
+      <NavbarMenu />
+    </div>
   );
 };
