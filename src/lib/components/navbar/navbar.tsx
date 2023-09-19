b923dd4682eed38f48c8467da3f901ddd47b8b6c
hcampos <hcampospro@gmail.com>
2023-09-19 18:21:41 +0200

    fix(navbar.tsx): fix typo in background color class name from "bg--500" to "bg-red-500" feat(navbar.tsx): add dark mode toggle button and conditional rendering for login and signup buttons based on connection status

--- src/lib/components/navbar/navbar.tsx
+++ src/lib/components/navbar/navbar.tsx
@@ -1,11 +1,33 @@
-import type { ReactElement } from "react";
+"use client";
+
+import { useState, type ReactElement } from "react";
 import { NavbarMenu } from "@/lib/components/navbar-menu/navbarMenu";
+import { Button } from "@/lib/components/ui/button";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
 
 export const Navbar = (): ReactElement => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isConnected, setIsConnected] = useState(true);
+
   return (
-    <div className="text-white bg-red-500 w-full mb-6 flex justify-between items-center">
+    <div className="text-white bg--500 w-full mb-10 flex justify-between items-center">
       <span style={{ filter: "drop-shadow(0px 5px 12px #ffffff)" }} className="text-2xl font-bold text-titleHighlight">LOLGAMES</span>
-      <NavbarMenu />
+      <div className="flex gap-2">
+        {isConnected && (
+          <>
+            <Button variant={"outline"} onClick={() => setIsDarkMode(!isDarkMode)}>
+              {isDarkMode ? <MoonIcon /> : <SunIcon />}
+            </Button>
+            <NavbarMenu />
+          </>
+        )}
+        {!isConnected && (
+          <>
+            <Button variant={"default"}>Log in</Button>
+            <Button variant={"default"}>Sign up</Button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
