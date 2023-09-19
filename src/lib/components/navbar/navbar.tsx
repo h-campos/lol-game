d79f064cf68c516662154ec39f57425cccfbaf10
hcampos <hcampospro@gmail.com>
2023-09-19 23:52:26 +0200

    chore(navbar.tsx): add link to home page in navbar to improve navigation

--- src/lib/components/navbar/navbar.tsx
+++ src/lib/components/navbar/navbar.tsx
@@ -4,13 +4,16 @@ import { useState, type ReactElement } from "react";
 import { NavbarMenu } from "@/lib/components/navbar-menu/navbarMenu";
 import { Button } from "@/lib/components/ui/button";
 import { ToggleTheme } from "../toggle-them/toggleTheme";
+import Link from "next/link";
 
 export const Navbar = (): ReactElement => {
   const [isConnected, setIsConnected] = useState(true);
 
   return (
     <div className="text-white bg--500 w-full mb-10 flex justify-between items-center">
-      <span style={{ filter: "drop-shadow(0px 5px 12px #ffffff)" }} className="text-2xl font-bold text-titleHighlight">LOLGAMES</span>
+      <Link href="/">
+        <span style={{ filter: "drop-shadow(0px 5px 12px #ffffff)" }} className="text-2xl font-bold text-titleHighlight">LOLGAMES</span>
+      </Link>
       <div className="flex gap-2">
         {isConnected && (
           <>
