c3c3df6ed23674b0fc8a17f225373a4444be553c
hcampos <hcampospro@gmail.com>
2023-09-20 21:05:37 +0200

    refactor(component.ts): make Props parameter optional in Component type to allow for components without props feat(getChampionsAssets.ts): add new function getChampionsAssetsWithName to retrieve champion assets by name

--- src/lib/utils/functions/getChampionsAssets.ts
+++ src/lib/utils/functions/getChampionsAssets.ts
@@ -5,3 +5,8 @@ export const getChampionsAssets = (champions: string[]): string => {
   const url = `http://ddragon.leagueoflegends.com/cdn/11.6.1/img/champion/${championsToShow}.png`;
   return url;
 };
+
+export const getChampionsAssetsWithName = (champions: string[], championName: string): string => {
+  const url = `http://ddragon.leagueoflegends.com/cdn/11.6.1/img/champion/${championName}.png`;
+  return url;
+};
