ef4363f77132339a219e29e842f79bd65b819643
hcampos <hcampospro@gmail.com>
2023-09-20 21:21:59 +0200

    feat(gamesList.ts): add gamesList data file to store information about available games
    The `gamesList.ts` file is added to the `src/lib/utils/data` directory. It defines a `GameList` type which represents the structure of each game in the list. The `gamesList` array contains information about available games, including their visual name, path, and status. Three games are initially included in the list: "Blurry Champions" with a path of "blurry-champions" and status "available", "Blurry Spells" with a path of "blurry-spells" and status "wip", and "Another One" with a path of "another-one" and status "unavailable".

---
+++ src/lib/utils/data/gamesList.ts
@@ -0,0 +1,23 @@
+type GameList = {
+  visualName: string;
+  path: string;
+  status: "available" | "unavailable" | "wip";
+}
+
+export const gamesList: GameList[] = [
+  {
+    visualName: "Blurry Champions",
+    path: "blurry-champions",
+    status: "available"
+  },
+  {
+    visualName: "Blurry Spells",
+    path: "blurry-spells",
+    status: "wip"
+  },
+  {
+    visualName: "Another One",
+    path: "another-one",
+    status: "unavailable"
+  }
+];
