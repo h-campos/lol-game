9d2f8eeb3c4754b05913ad8fc31591d3cd042957
hcampos <hcampospro@gmail.com>
2023-09-22 23:56:56 +0200

    chore(gamesList.ts): add databaseName property to GameList type for better data organization feat(gamesList.ts): add databaseName property to each game object in gamesList array to store the corresponding database name

--- src/lib/utils/data/gamesList.ts
+++ src/lib/utils/data/gamesList.ts
@@ -2,17 +2,20 @@ type GameList = {
   visualName: string;
   path: string;
   status: "available" | "unavailable" | "wip";
+  databaseName: string;
 }
 
 export const gamesList: GameList[] = [
   {
     visualName: "Blurry Champions",
     path: "blurry-champions",
+    databaseName: "blurryChampions",
     status: "available"
   },
   {
     visualName: "Blurry Spells",
     path: "blurry-spells",
+    databaseName: "blurrySpells",
     status: "wip"
   }
 ];
