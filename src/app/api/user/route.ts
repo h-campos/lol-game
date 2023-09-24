c0f882d543d86ddb3ba90f5f90ef26712e074027
hcampos <hcampospro@gmail.com>
2023-09-24 20:33:20 +0200

    fix(route.ts): return 401 status code with error message when user is not authorized to access the route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -6,7 +6,7 @@ import { prisma } from "@/lib/utils/database";
 export const GET = async(): Promise<NextResponse> => {
   const supabase = createRouteHandlerClient({ cookies });
   const { data: { user } } = await supabase.auth.getUser();
-  if (!user) return NextResponse.json({ data: null });
+  if (!user) return NextResponse.json({ error: "You are not authorized." }, { status: 401 });
 
   const data = await prisma.user.findUnique({
     where: { id: user.id }, include: { Games: true }
