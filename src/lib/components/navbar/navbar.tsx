2a13ab107d88f4cbeb75a78929cbf39d493f45c5
hcampos <hcampospro@gmail.com>
2023-09-25 12:41:25 +0200

    chore(navbar.tsx): add GitHub link button to the navbar for easy access to the project repository

--- src/lib/components/navbar/navbar.tsx
+++ src/lib/components/navbar/navbar.tsx
@@ -5,6 +5,8 @@ import { NavbarMenu } from "@/lib/components/navbar-menu/navbarMenu";
 import { ToggleTheme } from "../toggle-theme/toggleTheme";
 import Link from "next/link";
 import { useUserContext } from "@/lib/utils/contexts/user-provider";
+import { Button } from "../ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export const Navbar = (): ReactElement => {
   const { user } = useUserContext();
@@ -15,6 +17,9 @@ export const Navbar = (): ReactElement => {
         <span style={{ filter: "drop-shadow(0px 5px 12px #ffffff)" }} className="text-2xl font-bold text-titleHighlight">LOLGAMES</span>
       </Link>
       <div className="flex gap-2">
+        <Link target="_blank" href={"https://github.com/Sakoutecher/lol-game"}>
+          <Button variant={"outline"} size={"icon"}><GitHubLogoIcon className="transition-colors text-neutral-950 dark:text-neutral-50" /></Button>
+        </Link>
         {user && user !== "loading" && (
           <>
             <ToggleTheme />
