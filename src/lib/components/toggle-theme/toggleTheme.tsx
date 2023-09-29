76035ddb66c53c2038b0f2fe231d9ab588ac44e3
hcampos <hcampospro@gmail.com>
2023-09-29 12:20:44 +0200

    chore(toggleTheme.tsx): update button variant from "default" to "outline" for better visual consistency

--- src/lib/components/toggle-theme/toggleTheme.tsx
+++ src/lib/components/toggle-theme/toggleTheme.tsx
@@ -13,8 +13,7 @@ export const ToggleTheme = (): ReactElement => {
 
   return (
     <Button
-      className="sm:absolute sm:top-4 sm:right-4 mt-3 mr-3 sm:mt-0 sm:mr-0"
-      variant="default" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      variant="outline" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
       <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
