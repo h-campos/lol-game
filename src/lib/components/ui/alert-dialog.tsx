ceaefaca164804e7387b55822b5edb2d780400e3
hcampos <hcampospro@gmail.com>
2023-10-18 16:20:49 +0200

    refactor(alert-dialog.tsx): simplify AlertDialogPortal component by directly assigning AlertDialogPrimitive.Portal to it refactor(alert-dialog.tsx): remove unused props and displayName assignment in AlertDialogPortal component

--- src/lib/components/ui/alert-dialog.tsx
+++ src/lib/components/ui/alert-dialog.tsx
@@ -9,13 +9,7 @@ const AlertDialog = AlertDialogPrimitive.Root;
 
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 
-const AlertDialogPortal = ({
-  className,
-  ...props
-}: AlertDialogPrimitive.AlertDialogPortalProps): React.ReactElement => (
-  <AlertDialogPrimitive.Portal className={cn(className)} {...props} />
-);
-AlertDialogPortal.displayName = AlertDialogPrimitive.Portal.displayName;
+const AlertDialogPortal = AlertDialogPrimitive.Portal;
 
 const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
@@ -134,6 +128,8 @@ AlertDialogCancel.displayName = AlertDialogPrimitive.Cancel.displayName;
 
 export {
   AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
   AlertDialogTrigger,
   AlertDialogContent,
   AlertDialogHeader,
