b349365eee7e4ae9c35c4c5bde49834395fde36d
hcampos <hcampospro@gmail.com>
2023-11-08 18:27:36 +0100

    fix(getItemsData.ts): change URL protocol from HTTP to HTTPS to ensure secure connection when fetching item data from the League of Legends API

--- src/lib/utils/functions/getItemsData.ts
+++ src/lib/utils/functions/getItemsData.ts
@@ -2,7 +2,7 @@ import type { Items, ItemsData } from "@/config/types/itemsData.type";
 
 
 export const getItemsData = async(): Promise<Items[]> => {
-  const url = "http://ddragon.leagueoflegends.com/cdn/13.20.1/data/en_US/item.json";
+  const url = "https://ddragon.leagueoflegends.com/cdn/13.20.1/data/en_US/item.json";
   return await fetch(url).then((res) => res.json()).then((data: ItemsData) => {
     if (data && data.data) {
       const itemsArray = Object.values(data.data);
