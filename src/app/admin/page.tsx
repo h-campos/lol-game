bee63086554210aed6257e6898f66ee462611628
hcampos <hcampospro@gmail.com>
2023-11-08 18:19:23 +0100

    feat(admin): add AdminTable component to display report/proposal data in a table format
    feat(admin): add AdminTabs component to display tabs for different admin functionalities
    
    fix(admin): change fetcher function to use prisma to fetch proposal data from the database
    
    fix(admin): change deleteReport function to use prisma to delete a report/proposal from the database

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,18 @@
-"use client";
-
 import type { ReactElement } from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/lib/components/ui/tabs";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/lib/components/ui/table";
-import useSwr from "swr";
-import { fetcher } from "@/lib/utils/database/fetcher";
-import { Skeleton } from "@/lib/components/ui/skeleton";
-import type { Proposal } from "@prisma/client";
-import { Badge } from "@/lib/components/ui/badge";
-import { Button } from "@/lib/components/ui/button";
+import { prisma } from "@/lib/utils/database";
+import AdminTabs from "./lib/admin-tabs";
 
-const Admin = (): ReactElement => {
-  const { data, isLoading } = useSwr<Proposal[]>("/api/sendReportProposal", fetcher);
+const Admin = async(): Promise<ReactElement> => {
+  const data = await prisma.proposal.findMany();
+
+  const deleteReport = async(id: string): Promise<void> => {
+    "use server";
+    await prisma.proposal.delete({
+      where: {
+        id: id
+      }
+    });
+  };
 
   return (
     <>
@@ -20,46 +21,7 @@ const Admin = (): ReactElement => {
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">ADMIN DASHBOARD</h2>
           </div>
-          <Tabs defaultValue="report-proposal" className="space-y-4">
-            <TabsList>
-              <TabsTrigger value="report-proposal">Report / Proposal</TabsTrigger>
-              <TabsTrigger value="user-update">User Update</TabsTrigger>
-              <TabsTrigger value="new-game">New Game</TabsTrigger>
-            </TabsList>
-            <TabsContent value="report-proposal" className="space-y-4">
-              <div className="rounded-md border">
-                {isLoading && (<Skeleton className="rounded-md w-full h-12" />)}
-                {data && !isLoading && (
-                  <Table>
-                    <TableHeader>
-                      <TableRow>
-                        <TableHead>Pseudo</TableHead>
-                        <TableHead>Email</TableHead>
-                        <TableHead>Message</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead></TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      {data.map((report: Proposal, idx: number) => (
-                        <TableRow key={idx}>
-                          <TableCell>{report.username}</TableCell>
-                          <TableCell>{report.mail}</TableCell>
-                          <TableCell>{report.message}</TableCell>
-                          <TableCell><Badge variant={
-                            report.status === "To be reviewed" ? "destructive"
-                              : report.status === "In review" ? "yellow"
-                                : report.status === "Finish" ? "success" : "default"
-                          }>{report.status}</Badge></TableCell>
-                          <TableCell><Button variant={"outline"} size={"icon"}>...</Button></TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                )}
-              </div>
-            </TabsContent>
-          </Tabs>
+          <AdminTabs data={data} deleteReport={deleteReport} />
         </div>
       </div>
     </>
